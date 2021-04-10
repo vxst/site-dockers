63c7ab254310d3db82405206978cf59a043a0b13
Shan Chunqing <vxst@vxst.org>
2021-04-10 19:02:23 +0800

    Add vx.st container

---
+++ vx.st/html/setmail.js
@@ -0,0 +1,16 @@
+$("#vxstmail").click( function(e) {
+    var username = "vxst",
+        domain = "vxst.org";
+    window.location.href = "mailto:" + username + "@" + domain;
+} );
+$("#futurendamail").click( function(e) {
+    var username = "vxst",
+        domain = "futurenda.com";
+    window.location.href = "mailto:" + username + "@" + domain;
+} );
+$("#blog").click( function(e) {
+    window.location.href = "https://blog.vxst.org/";
+} );
+$("#github").click( function(e) {
+    window.location.href = "https://github.com/vxst";
+} );
